69039e183430063e558a63d3e91a4e7a3f5644bd
Kim Duclos <kimduclos@gmail.com>
2019-03-21 15:00:07 -0600

    set to deploy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const server = require('./api/server');
-const port = process.env.PORT || 4000;
+require('dotenv').config();
 
+const server = require('./api/server.js');
+
+// make the port dynamic
+const port = process.env.PORT || 4000;
+const greeting = process.env.GREETING;
 server.listen(port, () => {
-  console.info(`** Server Running on PORT ${port} **`)
+  console.log(`\n*** ${greeting} http://localhost:${port} ***\n`);
 });
